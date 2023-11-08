f06bdd693af5d3bb4cfa052d3f6ae7239388c448
Panos Young <pan107104@outlook.com>
2023-11-08 12:34:01 +0800

    Update olac.rb

--- Casks/olac.rb
+++ Casks/olac.rb
@@ -2,12 +2,12 @@ cask "olac" do
     arch = Hardware::CPU.intel? ? "intel" : "arm"
 
     if Hardware::CPU.intel?
-        sha256 "5332e1947eaf0b2bfe0b9213909b40f902c38b8c4010d357292e6fc36e0703ad"
+        sha256 "8593e52abfcc76659a75d2a9781b7e332f913d094b67ca7150e54bd25a780069"
     else
-        sha256 "38c988521a84c0276a0ec65431ac1b9dcb8452c634af6aa5de3e5632f1cdd204"
+        sha256 "99fe22f586a4c9f5da5d465a49fed4ae3ee8a71355a882f073db797b33a9dbd8"
     end
 
-    version "1.0.0"
+    version "0.1.0"
     url "https://github.com/Sin7Y/ola-lang/releases/download/v#{version}/olac-mac-#{arch}"
     name "Olac"
     desc "Olac is Ola language compiler."
